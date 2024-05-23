abad8c81fba54a2eb09832a44a28e03798f9cdbf
toandzpro <kimthoai15620@gmail.com>
2024-05-24 04:52:44 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+public class BàiTập8
+{
+    public static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.Write("Nhập tên của bạn: ");
+        string ten = Console.ReadLine();
+        Console.WriteLine("Chào bạn, " + ten + "!");
+    }
+}
